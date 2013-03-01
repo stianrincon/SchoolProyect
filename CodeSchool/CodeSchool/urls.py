9d035c21376dc834ec536b6b6718234a033339fc
stianrincon <stianrincon@outlook.com>
2013-03-01 13:43:05 -0800

    Cambio en la estructura del proyecto

--- CodeSchool/CodeSchool/urls.py
+++ CodeSchool/CodeSchool/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
+from CodeSchool import settings
 
 urlpatterns = patterns('',
     url(r'^$','WebSchool.views.login'),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+    {'document_root':settings.MEDIA_ROOT,}
+    ),
 )
