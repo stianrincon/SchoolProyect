1fcdcf961d7febb57df1052bf6421547065797a5
stianrincon <stianrincon@outlook.com>
2013-03-02 13:28:40 -0800

    evento del boton ingresa y creacion de welcome_administrator.html

--- CodeSchool/WebSchool/views.py
+++ CodeSchool/WebSchool/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-from django.contrib.auth.forms import AuthenticationForm
 
 def login(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-    else:
-        form = AuthenticationForm()        
-    return render_to_response('login.html', {'form':form}, context_instance = RequestContext(request))
+        return render_to_response('welcome_administrator.html', context_instance = RequestContext(request))
+    else: 
+        return render_to_response('login.html', context_instance = RequestContext(request))
