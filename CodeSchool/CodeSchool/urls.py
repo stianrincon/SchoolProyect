17b267e31409750b95811f15e5b542c671c5bf46
cristh18 <dar_funeral18@hotmail.com>
2013-03-04 16:20:45 -0500

    Mas cambios

--- CodeSchool/CodeSchool/urls.py
+++ CodeSchool/CodeSchool/urls.py
@@ -3,6 +3,7 @@ from CodeSchool import settings
 
 urlpatterns = patterns('',
     url(r'^$','WebSchool.views.login'),
+    url(r'^base_administrator/$','WebSchool.views.base_administrator'),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',
     {'document_root':settings.MEDIA_ROOT,}
     ),
--- CodeSchool/WebSchool/views.py
+++ CodeSchool/WebSchool/views.py
@@ -6,3 +6,7 @@ def login(request):
         return render_to_response('welcome_administrator.html', context_instance = RequestContext(request))
     else: 
         return render_to_response('login.html', context_instance = RequestContext(request))
+    
+def base_administrator(request):
+    return render_to_response('base_administrator.html',{'base_administrator':base_administrator}, context_instance=RequestContext(request))
+
